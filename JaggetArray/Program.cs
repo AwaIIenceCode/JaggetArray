22d7edb4187aa8a3dada295d2fc7b07ea3144750
AwallenceCode <awallencecode@gmail.com>
2025-08-26 15:21:15 +0300

    Initial commit

--- JaggetArray/Program.cs
+++ JaggetArray/Program.cs
@@ -1,2 +1,46 @@
-﻿
-Console.WriteLine("Hello, World!");
+﻿using System;
+class MyClass
+{
+    static void FillArray(int[][] userArr)
+    {
+        for (int i = 0; i < userArr.Length; i++)
+        {
+            for (int j = 0; j < userArr[i].Length; j++)
+            {
+                Console.Write($"Enter the {j + 1} number -> ");
+                string? input = Console.ReadLine();
+                
+                if (int.TryParse(input, out int value))
+                {
+                    userArr[i][j] = value;
+                }
+
+                else
+                {
+                    Console.WriteLine("Error. Must be a number, try again...");
+                    j--;
+                    continue;
+                }
+            }
+        }
+    }
+
+    static void Main()
+    {
+        int userSizeColumn, userSizeLine;
+        
+        Console.WriteLine("Enter the size column your array -> ");
+        if (!int.TryParse(Console.ReadLine(), out userSizeColumn) || userSizeColumn <= 0) { Console.WriteLine("Must be a positive number!"); return; }
+        int[][] userArr = new int[userSizeColumn][];
+
+        for (int i = 0; i < userSizeColumn; i++)
+        {
+            Console.WriteLine($"How many elements in column {i + 1}: ");
+            if (!int.TryParse(Console.ReadLine(), out userSizeLine) || userSizeLine <= 0) { Console.WriteLine("Must be a positive number!"); i--; continue; }
+
+            userArr[i] = new int[userSizeLine];
+        }
+        
+        FillArray(userArr);
+    }
+}
